761337bca303233f2ee88a5a01de1877a6674d10
vadimnovozhilov <novozhiloffvadim@gmail.com>
2020-01-22 22:06:02 +0300

    incorporated bangers font from google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Bangers');
+  body {
+    font-family: 'Bangers', sans-serif;
+  }
+`
 
 function App() {
   return (
-    <div>TOP CULTURE</div>
+    <div>
+      <GlobalStyles />
+      <h1>TOP CULTURE</h1>
+    </div>
   );
 }
 
